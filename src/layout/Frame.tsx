159f742ab4c13253b338d7dbaca98b0cb1386a92
Jason Healy <hello@jasonhealy.io>
2021-01-26 12:31:27 +0000

    build: settings screen

--- src/layout/Frame.tsx
+++ src/layout/Frame.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Layout, Row, Col } from 'antd'
+import '../theme/base.less'
+
 import Sidemenu from '../layout/SideMenu'
 import Connect from '../pages/Connect'
 import Connect2Col from '../pages/Connect2Col'
-import '../theme/base.less'
+import Settings from '../pages/Settings'
 
 const { Header, Content } = Layout
 
@@ -17,13 +19,8 @@ const Frame = () => {
         <Content className="main-content-container">
           <div style={{ margin: '0 auto', maxWidth: 960, padding: 20 }}>
             <Route path="/" exact component={Connect} />
+            <Route path="/settings" exact component={Settings} />
             <Route path="/2col" component={Connect2Col} />
-            {/* <Row>
-              <Col span={24} style={{ padding: '20px' }}>
-                <Route path="/" exact component={Connect} />
-                <Route path="/2col" component={Connect2Col} />
-              </Col>
-            </Row> */}
           </div>
         </Content>
       </Layout>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Typography, Form, Radio, Card } from 'antd'
+import Page from '../layout/Page'
+import { Content } from 'antd/lib/layout/layout'
+const { Title, Text } = Typography
 
 const Settings = () => {
-  return <Layout />
+  const [form] = Form.useForm()
+
+  return (
+    <Page
+      header={
+        <Content style={{ marginBottom: 30 }}>
+          <Title>Settings</Title>
+        </Content>
+      }
+    >
+      <Content style={{ marginBottom: 30 }}>
+        <Title level={5}>Viewing Mode</Title>
+        <Card>
+          <Radio.Group>
+            <Radio value="small">Light</Radio>
+            <Radio value="middle">System</Radio>
+            <Radio value="large">Dark</Radio>
+          </Radio.Group>
+        </Card>
+      </Content>
+
+      <Content>
+        <Title level={5}>Options</Title>
+        <Card>
+          <Radio.Group>
+            <Radio value="small">Light</Radio>
+            <Radio value="middle">System</Radio>
+            <Radio value="large">Dark</Radio>
+          </Radio.Group>
+        </Card>
+      </Content>
+    </Page>
+  )
 }
 
 export default Settings
