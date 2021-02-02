11619a910d3e9626bbf6ebdadf6da2975facb882
Jason Healy <hello@jasonhealy.io>
2021-02-02 16:03:52 +0000

    feat: set default agent

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import { Typography, Card, Button, Row, Space, Input } from 'antd'
+import { Typography, Card, Button, Row, Space, Tag } from 'antd'
 import Page from '../layout/Page'
 import { useVeramo } from '@veramo-community/veramo-react'
 import { DeleteOutlined } from '@ant-design/icons'
 const { Title, Text } = Typography
 
 const Agents = () => {
-  const { agents, removeAgent } = useVeramo()
+  const { agents, removeAgent, activeAgentId, setActiveAgentId } = useVeramo()
 
   const [names, setNames] = useState({})
 
@@ -15,14 +15,23 @@ const Agents = () => {
       {agents.map((agent) => {
         return (
           <Card key={agent.context.id}>
-            <Title>{agent.context.name}</Title>
+            <Row>
+              <Title>{agent.context.name}</Title>
+            </Row>
+            <Row style={{ marginBottom: 20 }}>
+              {activeAgentId === agent.context.id && (
+                <Tag color="geekblue">Default Agent</Tag>
+              )}
+              <Tag color="">Schema</Tag>
+              <Tag color="">W3C</Tag>
+            </Row>
             <Row style={{ marginBottom: 20 }}>
               <code>Schema: {agent.context.schema}</code>
             </Row>
-
             <Space>
               <Button
                 icon={<DeleteOutlined />}
+                disabled={activeAgentId === agent.context.id}
                 danger
                 onClick={() =>
                   agent.context.id && removeAgent(agent.context.id)
@@ -32,11 +41,12 @@ const Agents = () => {
               </Button>
               <Button
                 icon={<DeleteOutlined />}
+                disabled={activeAgentId === agent.context.id}
                 onClick={() =>
-                  agent.context.id && removeAgent(agent.context.id)
+                  agent.context.id && setActiveAgentId(agent.context.id)
                 }
               >
-                Save
+                Make Default
               </Button>
             </Space>
           </Card>
