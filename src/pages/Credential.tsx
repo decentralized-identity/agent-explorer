20f4676f1ef526e8e73b06444c7d1411030d9bf4
Jason Healy <hello@jasonhealy.io>
2021-02-05 14:53:59 +0000

    feat: stateful query module

--- src/pages/Credential.tsx
+++ src/pages/Credential.tsx
@@ -10,6 +10,8 @@ import {
   Table,
   Button,
   Input,
+  Select,
+  Space,
 } from 'antd'
 import { format } from 'date-fns'
 import Page from '../layout/Page'
@@ -23,6 +25,8 @@ import Chart from '../components/simple/Chart'
 import SubjectKey from '../components/widgets/SubjectKey'
 
 const { Title, Text } = Typography
+const { Option } = Select
+
 const data1 = {
   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
   fill: false,
@@ -79,6 +83,12 @@ const Credential = () => {
   )
 
   const [queryWidgetKey, setQueryWidgetKey] = useState<string>('')
+  const [queryModuleKey, setQueryModuleKey] = useState<string>('')
+  const [queryModuleIdentifier, setQueryModuleIdentifier] = useState(
+    credential?.credentialSubject.id,
+  )
+
+  const [userModules, setUserModules] = useState<any[]>([])
 
   const historyColumns = [
     {
@@ -118,6 +128,78 @@ const Credential = () => {
   const rightContent = () => {
     return (
       <Layout>
+        <Card title="Add query module">
+          <Space>
+            <Input
+              placeholder="claim"
+              onChange={(e) => setQueryModuleKey(e.target.value)}
+            />
+            <Select
+              defaultValue={queryModuleIdentifier}
+              style={{ width: 120 }}
+              onChange={(identifier) => setQueryModuleIdentifier(identifier)}
+            >
+              <Option value={credential?.credentialSubject.id as string}>
+                Subject
+              </Option>
+              <Option value={credential?.issuer.id as string}>Issuer</Option>
+            </Select>
+            <Button
+              onClick={() =>
+                setUserModules((s) =>
+                  s.concat([
+                    {
+                      id: new Date().toLocaleTimeString(),
+                      vcKey: queryModuleKey,
+                      subject: queryModuleIdentifier,
+                    },
+                  ]),
+                )
+              }
+            >
+              Add
+            </Button>
+          </Space>
+        </Card>
+        {userModules?.map(({ vcKey, id, subject }) => {
+          return (
+            <Card key={id} title={vcKey}>
+              <SubjectKey
+                did={subject}
+                vcKey={vcKey}
+                renderKey={(hash, data) => {
+                  return (
+                    <Row>
+                      <Col>
+                        {hash ? (
+                          <>
+                            <Text>
+                              Latest <b>{vcKey}</b> claim:
+                            </Text>
+                            {typeof data === 'string' ||
+                            typeof data === 'number' ? (
+                              <pre>
+                                <code>{data}</code>
+                              </pre>
+                            ) : (
+                              <pre>
+                                <code>{JSON.stringify(data, null, 2)}</code>
+                              </pre>
+                            )}
+                          </>
+                        ) : (
+                          <Text>
+                            No credentials for <b>{vcKey}</b>
+                          </Text>
+                        )}
+                      </Col>
+                    </Row>
+                  )
+                }}
+              />
+            </Card>
+          )
+        })}
         <Card title="Context">
           {credential?.['@context'].map((ctx: string) => {
             return (
@@ -137,6 +219,7 @@ const Credential = () => {
           title="Subject"
           identifier={credential?.credentialSubject.id as string}
         />
+
         <Card title="Subject Key Widget">
           <Text>
             This widget is for extracting specific credential claims where the
@@ -150,6 +233,7 @@ const Credential = () => {
             defaultValue="Enter claim type"
             onChange={(e) => setQueryWidgetKey(e.target.value)}
           />
+
           <SubjectKey
             did={credential?.credentialSubject.id as string}
             vcKey={queryWidgetKey}
