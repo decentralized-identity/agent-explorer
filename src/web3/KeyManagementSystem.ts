cbfb488e1ab2ea10c14365da90a4bc0760a44c35
Simonas Karuzas <simonas@not.cat>
2021-02-26 16:27:12 +0200

    feat: kms placeholder

---
+++ src/web3/KeyManagementSystem.ts
@@ -0,0 +1,62 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { TKeyType, IKey, EcdsaSignature } from '@veramo/core'
+import { AbstractKeyManagementSystem } from '@veramo/key-manager'
+
+export class Web3KeyManagementSystem extends AbstractKeyManagementSystem {
+  constructor(private provider: Promise<Web3Provider>) {
+    super()
+  }
+
+  async createKey({ type }: { type: TKeyType }): Promise<Omit<IKey, 'kms'>> {
+    throw Error('Not implemented')
+  }
+
+  async deleteKey(args: { kid: string }) {
+    // this kms doesn't need to delete keys
+    return true
+  }
+
+  async encryptJWE({
+    key,
+    to,
+    data,
+  }: {
+    key: IKey
+    to: IKey
+    data: string
+  }): Promise<string> {
+    throw Error('Not implemented')
+  }
+
+  async decryptJWE({
+    key,
+    data,
+  }: {
+    key: IKey
+    data: string
+  }): Promise<string> {
+    throw Error('Not implemented')
+  }
+
+  async signEthTX({
+    key,
+    transaction,
+  }: {
+    key: IKey
+    transaction: object
+  }): Promise<string> {
+    throw Error('Not implemented')
+    // this.provider.send
+    // return sign(transaction, '0x' + key.privateKeyHex)
+  }
+
+  async signJWT({
+    key,
+    data,
+  }: {
+    key: IKey
+    data: string
+  }): Promise<EcdsaSignature | string> {
+    throw Error('Not implemented')
+  }
+}
