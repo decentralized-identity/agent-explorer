28d5174e475a04b688a71da1320bff47192ca41d
Simonas Karuzas <simonas@not.cat>
2021-03-01 21:12:17 +0200

    feat: social post save to multiple agents

--- src/components/modules/PostForm.tsx
+++ src/components/modules/PostForm.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from 'react'
-import { Button, Card, Input } from 'antd'
+import { Button, Card, Form, Input, Select, Tag } from 'antd'
 import { useVeramo } from '@veramo-community/veramo-react'
-import { IDIDManager, IKey } from '@veramo/core'
-import { useWeb3React } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers'
+import { IDIDManager, IDataStore } from '@veramo/core'
 import shortId from 'shortid'
-import { sub } from 'date-fns'
 
 interface Props {
   id: string
 }
 
+interface FormValues {
+  headline: string
+  subject: string
+  articleBody: string
+  agents: string[]
+}
+
 const Module: React.FC<Props> = (props: Props) => {
-  const { agent } = useVeramo<IDIDManager>()
-  const [postId] = useState('https://example.org/posts/' + shortId())
-  const [subject, setSubject] = useState(postId)
-  const [headline, setHeadline] = useState('Headline')
-  const [articleBody, setArticleBody] = useState('Body')
+  const { agent, agents, getAgent } = useVeramo<IDIDManager & IDataStore>()
   const [vc, setVc] = useState()
 
-  const createPost = async () => {
+  const filteredAgents = agents.filter(a => a.availableMethods().includes('dataStoreSaveVerifiableCredential'))
+
+  const initialValues: FormValues = {
+    headline: 'Title',
+    subject: 'https://example.org/posts/' + shortId(),
+    articleBody: 'Body',
+    agents: []
+  }
+
+  const createPost = async (values: FormValues) => {
     try {
 
 
@@ -34,10 +43,10 @@ const Module: React.FC<Props> = (props: Props) => {
             'VerifiableCredential',
             'VerifiableSocialPosting'
           ],
-          id: postId,
+          id: initialValues.subject,
           issuanceDate: new Date().toISOString(),
           credentialSubject: {
-            id: subject,
+            id: values.subject,
             type: 'SocialMediaPosting',
             author: {
               id: props.id,
@@ -46,12 +55,22 @@ const Module: React.FC<Props> = (props: Props) => {
               image: 'https://google.com',
               name: 'cryptopunk'
             },
-            headline,
-            articleBody
+            headline: values.headline,
+            articleBody: values.articleBody
           },
         },
         proofFormat: 'jwt',
       })
+
+      for (const agentId of values.agents) {
+        try {
+          const result = await getAgent(agentId).dataStoreSaveVerifiableCredential({ verifiableCredential })
+          console.log({ result })
+        } catch (e) {
+          console.log(e)
+        }
+      }
+
       setVc(verifiableCredential)
     } catch (error) {
       console.log(error)
@@ -59,28 +78,88 @@ const Module: React.FC<Props> = (props: Props) => {
   }
 
 
+  function tagRender(props: any) {
+    const { label, closable, onClose } = props;
+
+    return (
+      <Tag closable={closable} onClose={onClose} style={{ marginRight: 3 }}>
+        {label}
+      </Tag>
+    );
+  }
+
+
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
 
   return (
     <Card title='Create post'>
 
-      <Input
-        placeholder='Subject'
-        value={subject}
-        onChange={(e) => setHeadline(e.target.value)}
-      />
-      <Input
-        placeholder='Headline'
-        value={headline}
-        onChange={(e) => setHeadline(e.target.value)}
-      />
-      <Input
-        placeholder='Article body'
-        value={articleBody}
-        onChange={(e) => setArticleBody(e.target.value)}
-      />
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={initialValues}
+        onFinish={createPost}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Subject"
+          name="subject"
+          rules={[{ required: true, message: 'Please input Subject!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Headline"
+          name="headline"
+          rules={[{ required: true, message: 'Please input Headline!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Article body"
+          name="articleBody"
+          rules={[{ required: true, message: 'Please input Article body!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Save to"
+          name="agents"
+        >
+          <Select
+            mode="multiple"
+            showArrow
+            tagRender={tagRender}
+            style={{ width: '100%' }}
+            options={filteredAgents.map(a => ({ label: a?.context.name as string, value: a?.context.id as string }))}
+          />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+        </Button>
+        </Form.Item>
+      </Form>
 
       {vc && (<pre>{JSON.stringify(vc, null, 2)}</pre>)}
-      <Button onClick={() => createPost()}>Add</Button>
     </Card>
 
   )
