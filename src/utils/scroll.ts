b29d26558ab2f55946b454a3230d15b7a8de32f5
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-06-06 22:50:53 -0400

    chore: remove leftover console log

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -1,6 +1,4 @@
 export const scrollMessages = (smooth?: boolean) => {
-  console.log(document.getElementById('chat-window')?.scrollHeight)
-
   document.getElementById('chat-window')?.scrollTo({
     top: document.getElementById('chat-window')?.scrollHeight,
     behavior: smooth ? 'smooth' : 'auto',
