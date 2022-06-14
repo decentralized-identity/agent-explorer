e93cad3438a726a5aa1ca9b1569add5c0f7a0a7d
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-06-14 13:47:55 -0400

    chore: cleanup

--- src/components/standard/IssueCredentialFromSchema.tsx
+++ src/components/standard/IssueCredentialFromSchema.tsx
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { Typography, Form, Input, Button, Select, Row, Card } from 'antd'
-import PageWidget from '../../layout/PageWidget'
-import { PageWidgetProps } from '../../types'
-import { issueCredential, claimToObject } from '../../utils/signing'
+import React, { useState } from 'react'
+import { Input, Button, Select, Card, Alert } from 'antd'
+import { issueCredential } from '../../utils/signing'
 import { useVeramo } from '@veramo-community/veramo-react'
 import { useQuery } from 'react-query'
-import { v4 as uuidv4 } from 'uuid'
 import { IIdentifier } from '@veramo/core'
-import JsonSchemaForm from '@rjsf/core'
+
+import { withTheme } from '@rjsf/core'
+import { Theme as AntDTheme } from '@rjsf/antd'
+import { VCJSONSchema } from '../../types'
+const JsonSchemaForm = withTheme(AntDTheme)
 
 const { Option } = Select
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 4 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 20 },
-  },
-}
-
-const schemasRepositoryUrlBase = 'https://veramolabs.github.io/vc-json-schemas'
 
 interface Field {
   type: string
@@ -30,23 +18,20 @@ interface Field {
 }
 
 interface IssueCredentialFromSchemaProps {
-  schema: JSON
+  schema: VCJSONSchema
 }
 
 const IssueCredentialFromSchema: React.FC<IssueCredentialFromSchemaProps> = ({
   schema,
 }) => {
   const { agent } = useVeramo()
-  const [claimType, setClaimType] = useState<string>('')
-  const [claimValue, setClaimValue] = useState<string>('')
-  const [credentialType, setCredentialType] = useState<string>('')
-  const [customContext, setCustomContext] = useState<string>('')
   const [errorMessage, setErrorMessage] = useState<null | string>()
   const [sending] = useState(false)
   const [issuer, setIssuer] = useState<string>('')
   const [subject, setSubject] = useState<string>()
-  // const [fields, updateFields] = useState<Field[]>([])
-  // const [somethingElse, updateSomethingElse] = useState<Field[]>([])
+
+  const [formData, setFormData] = useState<any>({})
+
   const [proofFormat, setProofFormat] = useState('jwt')
 
   const { data: identifiers, isLoading: identifiersLoading } = useQuery(
@@ -54,91 +39,97 @@ const IssueCredentialFromSchema: React.FC<IssueCredentialFromSchemaProps> = ({
     () => agent?.didManagerFind(),
   )
 
-  const signVc = async (fields: Field[], send?: boolean) => {
-    console.log('fields: ', fields)
-    const res = await issueCredential(
-      agent,
-      issuer,
-      subject,
-      fields,
-      proofFormat,
-      customContext,
-      credentialType,
-    )
-    console.log('res: ', res)
-    setIssuer('')
-    setSubject('')
-    // updateFields([])
+  const signVc = async (fields: Field[]) => {
+    try {
+      const res = await issueCredential(
+        agent,
+        issuer,
+        subject,
+        fields,
+        proofFormat,
+        schema.id,
+        schema.name,
+      )
+      console.log('res: ', res)
+      setIssuer('')
+      setSubject('')
+      setFormData({})
+    } catch (error: any) {
+      console.error('ex: ', error)
+      setErrorMessage(error.toString())
+    }
   }
 
   return (
     <Card>
-      <Typography.Text>Credential preview</Typography.Text>
-      <br />
-      <br />
-      <Form {...formItemLayout}>
-        <Form.Item>
-          <Input
-            value={subject}
-            placeholder="subject DID"
-            style={{ width: '60%' }}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Select
-            style={{ width: '60%' }}
-            loading={identifiersLoading}
-            onChange={(e) => setIssuer(e as string)}
-            placeholder="issuer DID"
-            defaultActiveFirstOption={true}
-          >
-            {identifiers &&
-              identifiers.map((id: IIdentifier) => (
-                <Option key={id.did} value={id.did as string}>
-                  {id.did}
-                </Option>
-              ))}
-          </Select>
-        </Form.Item>
-        <Form.Item>
-          <Select
-            style={{ width: '60%' }}
-            onChange={(e) => setProofFormat(e as string)}
-            placeholder="jwt or lds"
-            defaultActiveFirstOption={true}
-          >
-            <Option key={'jwt'} value="jwt">
-              jwt
-            </Option>
-            <Option key="lds" value="lds">
-              lds
-            </Option>
-          </Select>
-        </Form.Item>
-        <Form.Item>
-          <JsonSchemaForm
-            schema={(schema as any).schema}
-            onChange={(e) => {
-              console.log('onChange e: ', e)
-            }}
-            onSubmit={(e: any) => {
-              e.preventDefault()
-              console.log('e: ', e)
-              const fields: Field[] = []
-              for (let key in e.formData as any) {
-                console.log('key: ', key)
-                console.log('value: ', (e.formData as any)[key])
-                fields.push({ type: key, value: (e.formData as any)[key] })
-              }
-              //updateSomethingElse(fields);
-              console.log('e: ', e)
-              signVc(fields)
-            }}
-            onError={() => {}}
-          />
-        </Form.Item>
-      </Form>
+      <JsonSchemaForm
+        schema={(schema as any).schema}
+        formData={formData}
+        onChange={(e) => {
+          setFormData(e.formData)
+        }}
+        onError={() => {}}
+      >
+        <Input
+          value={subject}
+          placeholder="subject DID"
+          style={{ width: '60%' }}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+        <Select
+          style={{ width: '60%' }}
+          loading={identifiersLoading}
+          onChange={(e) => setIssuer(e as string)}
+          placeholder="issuer DID"
+          defaultActiveFirstOption={true}
+        >
+          {identifiers &&
+            identifiers.map((id: IIdentifier) => (
+              <Option key={id.did} value={id.did as string}>
+                {id.did}
+              </Option>
+            ))}
+        </Select>
+        <Select
+          style={{ width: '60%' }}
+          onChange={(e) => setProofFormat(e as string)}
+          placeholder="jwt or lds"
+          defaultActiveFirstOption={true}
+        >
+          <Option key={'jwt'} value="jwt">
+            jwt
+          </Option>
+          <Option key="lds" value="lds">
+            lds
+          </Option>
+        </Select>
+
+        <br />
+        <br />
+
+        <Button
+          type="primary"
+          onClick={() => {
+            setErrorMessage('')
+            const fields: Field[] = []
+            for (let key in formData as any) {
+              fields.push({ type: key, value: (formData as any)[key] })
+            }
+            signVc(fields)
+          }}
+          style={{ marginRight: 5 }}
+          disabled={sending || !subject || !issuer || !proofFormat}
+        >
+          Issue
+        </Button>
+        {errorMessage && (
+          <>
+            <br />
+            <br />
+            <Alert message={errorMessage} type="error" />
+          </>
+        )}
+      </JsonSchemaForm>
     </Card>
   )
 }
--- src/components/widgets/SelectSchemaAndIssue.tsx
+++ src/components/widgets/SelectSchemaAndIssue.tsx
@@ -1,33 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { Typography, Form, Input, Button, Select, Row } from 'antd'
+import { Select } from 'antd'
 import PageWidget from '../../layout/PageWidget'
-import { PageWidgetProps } from '../../types'
-import { issueCredential, claimToObject } from '../../utils/signing'
-import { useVeramo } from '@veramo-community/veramo-react'
-import { useQuery } from 'react-query'
-import { v4 as uuidv4 } from 'uuid'
-import { IIdentifier } from '@veramo/core'
+import { VCJSONSchema, PageWidgetProps } from '../../types'
 import IssueCredentialFromSchema from '../standard/IssueCredentialFromSchema'
 
 const { Option } = Select
 
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 4 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 20 },
-  },
-}
-
 const schemasRepositoryUrlBase = 'https://veramolabs.github.io/vc-json-schemas'
-
-interface Field {
-  type: string
-  value: any
-}
 
 interface SelectSchemaAndIssueProps extends PageWidgetProps {}
 
@@ -37,28 +16,12 @@ const SelectSchemaAndIssue: React.FC<SelectSchemaAndIssueProps> = ({
   remove,
   removeDisabled,
 }) => {
-  const { agent } = useVeramo()
-  const [claimType, setClaimType] = useState<string>('')
-  const [claimValue, setClaimValue] = useState<string>('')
-  const [credentialType, setCredentialType] = useState<string>('')
-  const [customContext, setCustomContext] = useState<string>('')
-  const [errorMessage, setErrorMessage] = useState<null | string>()
-  const [sending] = useState(false)
-  const [issuer, setIssuer] = useState<string>('')
-  const [subject, setSubject] = useState<string>()
-  const [fields, updateFields] = useState<Field[]>([])
-  const [proofFormat, setProofFormat] = useState('jwt')
-
-  const { data: identifiers, isLoading: identifiersLoading } = useQuery(
-    ['identifiers', { agentId: agent?.context.id }],
-    () => agent?.didManagerFind(),
-  )
-
   const [schemaUrlsList, setSchemaUrlsList] = useState<string[]>([])
   const [selectedSchemaUrl, setSelectedSchemaUrl] = useState<string>('')
   const [schemasLoading, setSchemasLoading] = useState(true)
-  const [selectedSchema, setSelectedSchema] = useState<JSON>()
+  const [selectedSchema, setSelectedSchema] = useState<VCJSONSchema>()
 
+  // load schemas from repo
   useEffect(() => {
     const getSchemas = async () => {
       const url = `${schemasRepositoryUrlBase}/index.json`
@@ -66,13 +29,13 @@ const SelectSchemaAndIssue: React.FC<SelectSchemaAndIssueProps> = ({
         method: 'GET',
       })
       const json = await response.json()
-      console.log('json.schemas: ', json.schemas)
       setSchemaUrlsList(json.schemas)
       setSchemasLoading(false)
     }
     getSchemas()
   }, [])
 
+  // load selected schema
   useEffect(() => {
     const getSchemas = async () => {
       const url = `${schemasRepositoryUrlBase}/${selectedSchemaUrl}`
@@ -80,29 +43,11 @@ const SelectSchemaAndIssue: React.FC<SelectSchemaAndIssueProps> = ({
         method: 'GET',
       })
       const json = await response.json()
-      console.log('json selected schema: ', json)
       setSelectedSchema(json)
     }
     getSchemas()
   }, [selectedSchemaUrl])
 
-  const signVc = async (send?: boolean) => {
-    await issueCredential(
-      agent,
-      issuer,
-      subject,
-      fields,
-      proofFormat,
-      customContext,
-      credentialType,
-    )
-
-    setIssuer('')
-    setSubject('')
-    updateFields([])
-  }
-
-  console.log('schemas: ', schemaUrlsList)
   return (
     <PageWidget
       title={title}
