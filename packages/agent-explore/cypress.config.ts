0f0f5def8934a59d6f353beb2b1c0813e0363ec0
Simonas Karuzas <simonas@not.cat>
2023-10-18 16:28:19 +0300

    fix: gh

--- packages/agent-explore/cypress.config.ts
+++ packages/agent-explore/cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "21c5nu",
   video: true,
+  viewportWidth: 800,
+  viewportHeight: 550,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
