d01598647af3569b03f9843ba28662ee1c4784bf
Mircea Nistor <mirceanis@gmail.com>
2023-10-18 17:01:13 +0200

    fix(ci): change server command for cypress

--- packages/agent-explore/cypress/e2e/discovery.cy.ts
+++ packages/agent-explore/cypress/e2e/discovery.cy.ts
@@ -1,6 +1,6 @@
 describe('Discovery shows BrainShare home page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:8000/')
     cy.get('#rc_select_0').click().type('{enter}')
     cy.wait(3000)
     cy.contains('BrainShare')
