0594010b7c534ed8dee138bd1cd423c9f846b299
Mircea Nistor <mirceanis@gmail.com>
2023-10-18 17:12:24 +0200

    fix(ci): port number typo

--- packages/agent-explore/cypress/e2e/discovery.cy.ts
+++ packages/agent-explore/cypress/e2e/discovery.cy.ts
@@ -1,6 +1,6 @@
 describe('Discovery shows BrainShare home page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8000/')
+    cy.visit('http://localhost:8080/')
     cy.get('#rc_select_0').click().type('{enter}')
     cy.wait(3000)
     cy.contains('BrainShare')
