f2f4843e0e1e8b38841ea7497661704d0008fabf
Mircea Nistor <mirceanis@gmail.com>
2023-10-18 17:18:52 +0200

    fix(ci): revert to using pnpm run dev

--- packages/agent-explore/cypress/e2e/discovery.cy.ts
+++ packages/agent-explore/cypress/e2e/discovery.cy.ts
@@ -1,6 +1,6 @@
 describe('Discovery shows BrainShare home page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('http://localhost:3000/')
     cy.get('#rc_select_0').click().type('{enter}')
     cy.wait(3000)
     cy.contains('BrainShare')
