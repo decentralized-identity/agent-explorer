dd5fe2f1a1f2f9c11b3acdbae0f0e12abb5465e8
Simonas Karuzas <simonas@not.cat>
2023-10-19 16:55:14 +0300

    feat: demo with gitcoin passport

--- packages/agent-explore/cypress/e2e/discovery.cy.ts
+++ packages/agent-explore/cypress/e2e/discovery.cy.ts
@@ -5,7 +5,7 @@ describe('Discovery shows BrainShare home page', () => {
     cy.wait(2000)
 
     cy.get('#rc_select_0').click().type('{enter}')
-    cy.wait(2000)
+    cy.wait(4000)
 
     cy.contains('DID Document').click()
     cy.wait(2000)
@@ -15,40 +15,17 @@ describe('Discovery shows BrainShare home page', () => {
     cy.wait(2000)
     cy.get(':nth-child(2) > .ant-pro-list-row-header > .ant-list-item-meta > .ant-list-item-meta-content > .ant-list-item-meta-title > .ant-pro-list-row-header-container > .ant-pro-list-row-title > .ant-btn > span').click();
     cy.wait(2000)
-    cy.contains('Contacts').click()
+    
+    // cy.contains('Discover').click() // FIXME: this is not working properly
+    cy.visit('http://localhost:3000/')
+    const input = cy.get('#rc_select_0').click().clear().type('Nick')
+    cy.wait(3000)
+    input.type('{downArrow}')
+    cy.wait(1000)
+    input.type('{enter}')
     cy.wait(2000)
-    cy.contains('VeramoLabs').click()
-    cy.wait(2000)
+    cy.contains('Gitcoin passport').click()
+    cy.wait(8000)
 
-    // cy.contains('Veramo community').click()
-    // cy.wait(2000)
-    // cy.contains('Private').click()
-    // cy.wait(2000)
-    // cy.contains('Identifiers').click()
-    // cy.wait(2000)
-    // cy.contains('New').click()
-    // cy.wait(2000)
-    // cy.get('#form_in_form_alias').clear();
-    // cy.get('#form_in_form_alias').type('alice');
-    // cy.wait(2000)
-    // cy.contains('Next').click()
-    // cy.wait(2000)
-    // cy.contains('Finish').click()
-    // cy.wait(2000)
-    // cy.contains('alice').click()
-    // cy.wait(2000)
-    // cy.contains('Profile').click();
-    // cy.wait(1000)
-    // cy.contains('Edit').click();
-    // cy.wait(1000)
-    // cy.get('#form_in_form_name').clear();
-    // cy.wait(1000)
-    // cy.get('#form_in_form_name').type('Alice');
-    // cy.wait(1000)
-    // cy.get('#form_in_form_email').clear();
-    // cy.wait(1000)
-    // cy.get('#form_in_form_email').type('alice@wonderland.com');
-    // cy.contains('Save to: Private').click();
-    // cy.wait(1000)
   })
 })
